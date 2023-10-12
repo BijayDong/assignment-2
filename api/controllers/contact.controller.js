fa70ef9969656893ea97afbd7a0003cac79f8d68
Bijay Tamang <21167543@students.ltu.edu.au>
2023-10-12 17:35:56 +1100

    contact.controller.js file modification

--- api/controllers/contact.controller.js
+++ api/controllers/contact.controller.js
@@ -4,7 +4,30 @@ const Phones = db.phones;
 const Op = db.Sequelize.Op;
 
 // Create contact
-exports.create = (req, res) => {};
+exports.create = (req, res) => {
+  // validate request
+  if (!req.body.name) {
+    res.status(400).send({
+      message: "Name cannot be empty",
+    });
+    return;
+  }
+  // create a contact object
+  const contact = {
+    name: req.body.name,
+  };
+
+  // save to database
+  Contacts.create(contact)
+    .then((data) => {
+      res.send(data);
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message: err.message || "Error occurred while creating the contact.",
+      });
+    });
+};
 
 // Get all contacts
 exports.findAll = (req, res) => {
@@ -20,10 +43,72 @@ exports.findAll = (req, res) => {
 };
 
 // Get one contact by id
-exports.findOne = (req, res) => {};
+exports.findOne = (req, res) => {
+  const id = req.params.id;
+
+  Contacts.findByPk(id)
+    .then((data) => {
+      if (data) {
+        res.send(data);
+      } else {
+        res.status(404).send({
+          message: `Cannot find contact with id=${id}.`,
+        });
+      }
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message: `Error retrieving contact with id=${id}`,
+      });
+    });
+};
 
 // Update one contact by id
-exports.update = (req, res) => {};
+exports.update = (req, res) => {
+  const id = req.params.id;
+
+  Contacts.update(req.body, {
+    where: { id: id },
+  })
+    .then((num) => {
+      if (num == 1) {
+        res.send({
+          message: "Contact was updated successfully.",
+        });
+      } else {
+        res.send({
+          message: `Cannot update contact with id=${id}. Maybe contact was not found or req.body is empty!`,
+        });
+      }
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message: `Error updating contact with id=${id}`,
+      });
+    });
+};
 
 // Delete one contact by id
-exports.delete = (req, res) => {};
+exports.delete = (req, res) => {
+  const id = req.params.id;
+
+  Contacts.destroy({
+    where: { id: id },
+  })
+    .then((num) => {
+      if (num == 1) {
+        res.send({
+          message: "Contact was deleted successfully!",
+        });
+      } else {
+        res.send({
+          message: `Cannot delete contact with id=${id}. Maybe contact was not found!`,
+        });
+      }
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message: `Error deleting contact with id=${id}`,
+      });
+    });
+};
